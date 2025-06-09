51fa1a9b864b2f59009231b787a5aa2a5371fbe9
Dibyajyoti7640 <sahoodibya2408@gmail.com>
2025-06-09 18:51:27 +0530

    secret

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
               </PrivateRoute>
             }
           />
+          {console.log("Shhhhhhh its a secret")}
           <Route
             path="admin/courses/:courseId"
             element={
